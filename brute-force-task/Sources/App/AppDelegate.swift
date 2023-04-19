ff39f3c44b7341a991e22be6614e43269c24af69
ti1ek <tilek.koszhanov@mail.ru>
2023-04-20 01:32:51 +0500

    Project settings

---
+++ brute-force-task/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  brute-force-task
+//
+//  Created by Zhuldyz Bukeshova on 20.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
