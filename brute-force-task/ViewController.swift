19f7b2ced8f1b571d00b2e005949daa4be31c740
ti1ek <tilek.koszhanov@mail.ru>
2023-04-20 01:13:17 +0500

    Initial Commit

---
+++ brute-force-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  brute-force-task
+//
+//  Created by Zhuldyz Bukeshova on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
